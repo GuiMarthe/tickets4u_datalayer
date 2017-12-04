95607b12a770f5910da81fb2247ad0d404118db6
Gui <gui.marthe@gmail.com>
2017-12-04 15:45:55 -0200

    added new population phase to bash script

--- reset_and_populate.sh
+++ reset_and_populate.sh
@@ -1,3 +1,10 @@
 psql tickets4u -f população_db/drop_db.sql;
 psql tickets4u -f população_db/create_db.sql;
 psql tickets4u -f população_db/popula_empresa_evento.sql;
+psql tickets4u -f população_db/popula_subclasse_eventos.sql;
+#todo sessao
+#todo local
+#todo grupo de ingressos
+#todo bilhete 
+psql tickets4u -f população_db/popule_clientes.sql;
+#todo compra
